d8dc90eab6b95dd5645309952d82133656036353
Dawid Walczak <dawidwalczak1404@gmail.com>
2024-04-16 00:12:32 +0200

    add Flight and AircraftType entity classes

---
+++ FlightManager.Core/Domain/Entities/AircraftType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightManager.Core.Domain.Entities
+{
+    /// <summary>
+    /// Aircraft type model for managing database operations
+    /// </summary>
+    public class AircraftType
+    {
+        [Key]
+        public Guid ID { get; set; }
+
+        [StringLength(60)]
+        public string? Name { get; set; }
+    }
+}
---
+++ FlightManager.Core/Domain/Entities/Flight.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightManager.Core.Domain.Entities
+{
+    /// <summary>
+    /// Flight model for managing database operations
+    /// </summary>
+    public class Flight
+    {
+        [Key]
+        public Guid FlightID { get; set; }
+
+        public int? Number { get; set; }
+
+        public DateTime? DepartureDate { get; set; }
+
+        [StringLength(100)]
+        public string? DepartureAirport { get; set; }
+
+        [StringLength(100)]
+        public string? ArrivalAirport { get; set; }
+
+        public Guid? AircraftTypeID { get; set; }
+    }
+}
